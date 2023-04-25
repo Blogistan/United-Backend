afc1c0c25e66b742c0b194b43a9d06e5683639eb
Esquetta <esquetta@gmail.com>
2023-04-25 20:39:44 +0300

    ReactionType Enum added.

---
+++ Domain/Domain/Enums/ReactionTypes.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Enums
+{
+    public enum ReactionTypes
+    {
+        Suprised = 0,//Yog Amına
+        Sad = 1,//Sad Post
+        KEKW = 2,//Kahkaha Tufanı
+        Lovely = 3,//Galp
+        Triggered = 4//Kudurdum
+
+
+    }
+}
