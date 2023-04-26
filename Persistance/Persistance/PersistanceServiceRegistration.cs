9582e6734e5a4929b4c13a9949cf242c8787e218
Esquetta <esquetta@gmail.com>
2023-04-26 21:09:02 +0300

    Service registration class added.

---
+++ Persistance/Persistance/PersistanceServiceRegistration.cs
@@ -0,0 +1,28 @@
+﻿using Application.Services.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistance.Repositories;
+
+namespace Persistance
+{
+    public static class PersistanceServiceRegistration
+    {
+        public static IServiceCollection AddPersistanceSerices(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddSingleton<IBlogRepository, BlogRepository>();
+            services.AddSingleton<ICategoryRepository, CategoryRepository>();
+            services.AddSingleton<ICommentRepository, CommentRepository>();
+            services.AddSingleton<IContentRepository, ContentRepository>();
+            services.AddSingleton<IEmailAuthenticatorRepository, EmailAuthenticatorRepository>();
+            services.AddSingleton<IOperationClaimRepostiory, OperationClaimRepository>();
+            services.AddSingleton<IOtpAuthenticatorRepository, OtpAuthenticatorRepository>();
+            services.AddSingleton<IRefreshTokenRepository, RefreshTokenRepository>();
+            services.AddSingleton<IUserOperationClaimRepository, UserOperationClaimRepository>();
+            services.AddSingleton<IUserRepository, UserRepository>();
+            services.AddSingleton<IVideoRepository, VideoRepository>();
+            services.AddScoped<ISiteUserRepository, SiteUserRepository>();
+
+            return services;
+        }
+    }
+}
--- Persistance/Persistance/Repositories/SiteUserRepository.cs
+++ Persistance/Persistance/Repositories/SiteUserRepository.cs
@@ -1,10 +1,11 @@
-﻿using Core.Persistence.Repositories;
+﻿using Application.Services.Repositories;
+using Core.Persistence.Repositories;
 using Domain.Entities;
 using Persistance.Context;
 
 namespace Persistance.Repositories
 {
-    public class SiteUserRepository : EfRepositoryBase<SiteUser, int, EFDbContext>
+    public class SiteUserRepository : EfRepositoryBase<SiteUser, int, EFDbContext>,ISiteUserRepository
     {
         public SiteUserRepository(EFDbContext context) : base(context)
         {
