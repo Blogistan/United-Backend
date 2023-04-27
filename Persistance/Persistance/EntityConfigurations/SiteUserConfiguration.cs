80f91929f3715cb6de34dd5fb0dc2c06604c965b
Esquetta <esquetta@gmail.com>
2023-04-27 20:06:08 +0300

    SiteUser entity configuration added.

---
+++ Persistance/Persistance/EntityConfigurations/SiteUserConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistance.EntityConfigurations
+{
+    public class SiteUserConfiguration : IEntityTypeConfiguration<SiteUser>
+    {
+        public void Configure(EntityTypeBuilder<SiteUser> builder)
+        {
+            builder.ToTable("Users");
+
+        }
+    }
+}
