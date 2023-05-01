05b00defd9345a3e85b11d7ec4d575391d9f0ffb
Esquetta <esquetta@gmail.com>
2023-05-01 17:15:11 +0300

    Verify command added.

---
+++ Application/Application/Features/Auth/Commands/VerifyEmailAuthenticatorCommand/VerifyEmailAuthenticatorCommand.cs
@@ -0,0 +1,44 @@
+﻿using Application.Features.Auth.Rules;
+using Application.Services.Repositories;
+using Core.Security.Entities;
+using Core.Security.Enums;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.Auth.Commands.VerifyEmailAuthenticatorCommand
+{
+    public class VerifyEmailAuthenticatorCommand : IRequest
+    {
+        public string ActivationKey { get; set; }
+        public string[] Roles => Array.Empty<string>();
+
+        public class VerifyEmailAuthenticatorCommandHandler : IRequestHandler<VerifyEmailAuthenticatorCommand>
+        {
+            private readonly IEmailAuthenticatorRepository emailAuthenticatorRepository;
+            private readonly AuthBussinessRules authBussinessRules;
+            public VerifyEmailAuthenticatorCommandHandler(IEmailAuthenticatorRepository emailAuthenticatorRepository, AuthBussinessRules authBussinessRules)
+            {
+                this.emailAuthenticatorRepository = emailAuthenticatorRepository;
+                this.authBussinessRules = authBussinessRules;
+            }
+            public async Task Handle(VerifyEmailAuthenticatorCommand request, CancellationToken cancellationToken)
+            {
+                EmailAuthenticator emailAuthenticator = await emailAuthenticatorRepository.GetAsync(x => x.ActivationKey == request.ActivationKey, include: x => x.Include(x => x.User));
+
+
+                await authBussinessRules.UserEmailAuthenticatorShouldBeExists(emailAuthenticator);
+
+                await VerifyEmailAuthenticator(emailAuthenticator);
+                return Task.CompletedTask;
+            }
+
+            private async Task VerifyEmailAuthenticator(EmailAuthenticator emailAuthenticator)
+            {
+                emailAuthenticator.IsVerified = true;
+                emailAuthenticator.ActivationKey = null;
+                emailAuthenticator.User.AuthenticatorType = AuthenticatorType.Email;
+                await emailAuthenticatorRepository.UpdateAsync(emailAuthenticator);
+            }
+        }
+    }
+}
---
+++ Application/Application/Features/Auth/Commands/VerifyOtpAuthenticatorCommand/VerifyOtpAuthenticatorCommand.cs
@@ -0,0 +1,54 @@
+﻿using Application.Features.Auth.Rules;
+using Application.Services.Auth;
+using Application.Services.Repositories;
+using Core.Application.Pipelines.Authorization;
+using Core.Security.Entities;
+using Core.Security.Enums;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SharpCompress.Compressors.Xz;
+
+namespace Application.Features.Auth.Commands.VerifyOtpAuthenticatorCommand
+{
+    public class VerifyOtpAuthenticatorCommand : IRequest, ISecuredRequest
+    {
+        public int UserId { get; set; }
+        public string OtpCode { get; set; }
+
+        public string[] Roles => Array.Empty<string>();
+
+    }
+
+    public class VerifyOtpAuthenticatorCommandHandler : IRequestHandler<VerifyOtpAuthenticatorCommand>
+    {
+        private IOtpAuthenticatorRepository otpAuthenticatorRepository;
+        private AuthBussinessRules authBussinessRules;
+        private IAuthService authService;
+        public VerifyOtpAuthenticatorCommandHandler(IOtpAuthenticatorRepository otpAuthenticatorRepository, AuthBussinessRules authBussinessRules, IAuthService authService)
+        {
+            this.otpAuthenticatorRepository = otpAuthenticatorRepository;
+            this.authService = authService;
+            this.authBussinessRules = authBussinessRules;
+        }
+
+        public async Task Handle(VerifyOtpAuthenticatorCommand request, CancellationToken cancellationToken)
+        {
+            OtpAuthenticator otpAuthenticator = await otpAuthenticatorRepository.GetAsync(predicate: x => x.UserId == request.UserId, include: x => x.Include(x => x.User));
+
+            await authBussinessRules.UserOtpAuthenticatorShouldBeExists(otpAuthenticator);
+
+
+            otpAuthenticator!.User.AuthenticatorType = AuthenticatorType.Otp;
+
+            await authService.VerifyAuthenticatorCode(otpAuthenticator.User, request.OtpCode);
+            otpAuthenticator.IsVerified = true;
+
+            await otpAuthenticatorRepository.UpdateAsync(otpAuthenticator);
+
+            return Task.CompletedTask;
+        }
+    }
+
+
+
+}
