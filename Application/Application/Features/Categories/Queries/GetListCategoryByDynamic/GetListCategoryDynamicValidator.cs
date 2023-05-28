b60db51cc0050c97e9537161579165a788cc0ab6
Esquetta <esquetta@gmail.com>
2023-05-28 16:47:22 +0300

    Query validator added.

---
+++ Application/Application/Features/Categories/Queries/GetListCategoryByDynamic/GetListCategoryDynamicValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Categories.Queries.GetListCategoryByDynamic
+{
+    public class GetListCategoryDynamicValidator:AbstractValidator<GetListCategoryQueryByDynamicQuery>
+    {
+        public GetListCategoryDynamicValidator()
+        {
+            RuleFor(x => x.PageRequest).NotEmpty();
+           
+        }
+    }
+}
