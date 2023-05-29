5037733e8d9e1be947fe9d4a7588d6575278f1c5
Esquetta <esquetta@gmail.com>
2023-05-29 20:07:37 +0300

    Read blog query added.

---
+++ Application/Application/Features/Blogs/Queries/LikeBlog/ReadBlogQuery.cs
@@ -0,0 +1,37 @@
+﻿using Application.Features.Blogs.Dtos;
+using Application.Features.Blogs.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using MediatR;
+
+namespace Application.Features.Blogs.Queries.LikeBlog
+{
+    public class ReadBlogQuery:IRequest<BlogListViewDto>
+    {
+        public int BlogId { get; set; }
+
+        public class LikeBlogQueryHandler:IRequestHandler<ReadBlogQuery, BlogListViewDto>
+        {
+            private readonly IBlogRepository blogRepository;
+            private readonly IMapper mapper;
+            private readonly BlogBusinessRules blogBusinessRules;
+            public LikeBlogQueryHandler(IBlogRepository blogRepository, IMapper mapper, BlogBusinessRules blogBusinessRules)
+            {
+                this.blogRepository = blogRepository;
+                this.mapper = mapper;
+                this.blogBusinessRules = blogBusinessRules;
+            }
+
+            public async Task<BlogListViewDto> Handle(ReadBlogQuery request, CancellationToken cancellationToken)
+            {
+                var blog = await blogBusinessRules.BlogCheckById(request.BlogId);
+
+                blog.ReadCount++;
+
+                BlogListViewDto blogListViewDto = mapper.Map<BlogListViewDto>(blog);
+
+                return blogListViewDto;
+            }
+        }
+    }
+}
---
+++ Application/Application/Features/Blogs/Queries/LikeBlog/ReadBlogQueryValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Blogs.Queries.LikeBlog
+{
+    public class ReadBlogQueryValidator:AbstractValidator<ReadBlogQuery>
+    {
+        public ReadBlogQueryValidator()
+        {
+            RuleFor(x=>x.BlogId).NotEmpty();
+        }
+    }
+}
