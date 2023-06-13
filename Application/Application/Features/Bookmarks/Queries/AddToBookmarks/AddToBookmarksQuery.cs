176679fc8c1259c6d4d4000782ee7b1b14cb1554
Esquetta <esquetta@gmail.com>
2023-06-13 21:19:43 +0300

    AddToBookmarks class created.

---
+++ Application/Application/Features/Bookmarks/Queries/AddToBookmarks/AddToBookmarksQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace Application.Features.Bookmarks.Queries.AddToBookmarks
+{
+    public class AddToBookmarksQuery:IRequest<Unit>
+    {
+        public int BlogId { get; set; }
+        public int UserId { get; set; }
+
+
+        public class AddToBookmarksQueryHandler : IRequestHandler<AddToBookmarksQuery, Unit>
+        {
+            public async Task<Unit> Handle(AddToBookmarksQuery request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
