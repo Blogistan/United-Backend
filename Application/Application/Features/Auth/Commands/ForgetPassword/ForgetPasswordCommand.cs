0e04192726fb277c6236bedf092d03a1d0ec5b30
Esquetta <esquetta@gmail.com>
2023-07-22 16:22:14 +0300

    Forget Password command added.

---
+++ Application/Application/Features/Auth/Commands/ForgetPassword/ForgetPasswordCommand.cs
@@ -0,0 +1,59 @@
+﻿using Application.Features.Auth.Rules;
+using Application.Services.Repositories;
+using Core.Mailing;
+using Domain.Entities;
+using MediatR;
+using MimeKit;
+using System.Security.Policy;
+using System.Text;
+using System.Web;
+
+namespace Application.Features.Auth.Commands.ForgetPassword
+{
+    public class ForgetPasswordCommand : IRequest<Unit>
+    {
+        public string Email { get; set; }
+        public string PasswordResetUrl { get; set; }
+        public class ForgetPasswordCommandHandler : IRequestHandler<ForgetPasswordCommand, Unit>
+        {
+            private readonly ISiteUserRepository siteUserRepository;
+            private readonly IMailService mailService;
+            private readonly AuthBussinessRules authBussinessRules;
+            public ForgetPasswordCommandHandler(ISiteUserRepository siteUserRepository, IMailService mailService, AuthBussinessRules authBussinessRules)
+            {
+                this.siteUserRepository = siteUserRepository;
+                this.mailService = mailService;
+                this.authBussinessRules = authBussinessRules;
+            }
+
+            public async Task<Unit> Handle(ForgetPasswordCommand request, CancellationToken cancellationToken)
+            {
+                var user = await siteUserRepository.GetAsync(x => x.Email == request.Email);
+                await authBussinessRules.UserShouldBeExist(user);
+
+                await SendForgotPasswordMail(user, request.PasswordResetUrl);
+
+                return Unit.Value;
+            }
+            private async Task SendForgotPasswordMail(SiteUser siteUser, string passwordResetUrl)
+            {
+                List<MailboxAddress> mailboxAddresses = new List<MailboxAddress>();
+                mailboxAddresses.Add(new MailboxAddress(Encoding.UTF8, $"{siteUser.FirstName} {siteUser.LastName}", siteUser.Email));
+
+                var user = HttpUtility.
+                
+
+                Mail mail = new()
+                {
+                    Subject = "Forgot Password",
+                    ToList = mailboxAddresses,
+                    HtmlBody = $"Hi {siteUser.FirstName} {siteUser.LastName} " +
+                    $"Here is your password reset link " +
+                    $"{passwordResetUrl}?user={user}"
+                };
+
+                await mailService.SendEmailAsync(mail);
+            }
+        }
+    }
+}
---
+++ Application/Application/Features/Auth/Commands/ForgetPassword/ForgetPasswordValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Auth.Commands.ForgetPassword
+{
+    public class ForgetPasswordValidator : AbstractValidator<ForgetPasswordCommand>
+    {
+        public ForgetPasswordValidator()
+        {
+            RuleFor(x => x.Email).EmailAddress();
+        }
+    }
+}
