5ce3e9df652c98cc43947690fb581c8c2d8818fc
Esquetta <esquetta@gmail.com>
2023-07-25 20:41:13 +0300

    Send Password change mail func added.

--- Application/Application/Features/Auth/Commands/PasswordReset/PasswordResetCommand.cs
+++ Application/Application/Features/Auth/Commands/PasswordReset/PasswordResetCommand.cs
@@ -1,11 +1,15 @@
 ﻿using Application.Features.Auth.Rules;
 using Application.Services.Auth;
 using Application.Services.Repositories;
+using Core.Mailing;
 using Core.Security.Entities;
 using Core.Security.Hashing;
 using Domain.Entities;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using MimeKit;
+using System.Text;
 
 namespace Application.Features.Auth.Commands.PasswordReset
 {
@@ -22,18 +26,22 @@ namespace Application.Features.Auth.Commands.PasswordReset
             private readonly ISiteUserRepository siteUserRepository;
             private readonly AuthBussinessRules authBussinessRules;
             private readonly IForgotPasswordRepository forgotPasswordRepository;
-            //To Do:Password reset mail 
-            public PasswordResetCommandHandler(IAuthService authService, ISiteUserRepository siteUserRepository, AuthBussinessRules authBussinessRules, IForgotPasswordRepository forgotPasswordRepository)
+            private readonly IMailService mailService;
+            private readonly IHttpContextAccessor httpContextAccessor;
+
+            public PasswordResetCommandHandler(IAuthService authService, ISiteUserRepository siteUserRepository, AuthBussinessRules authBussinessRules, IForgotPasswordRepository forgotPasswordRepository, IMailService mailService, IHttpContextAccessor httpContextAccessor)
             {
                 this.authService = authService;
                 this.siteUserRepository = siteUserRepository;
                 this.authBussinessRules = authBussinessRules;
                 this.forgotPasswordRepository = forgotPasswordRepository;
+                this.mailService = mailService;
+                this.httpContextAccessor = httpContextAccessor;
             }
 
             public async Task<Unit> Handle(PasswordResetCommand request, CancellationToken cancellationToken)
             {
-                ForgotPassword forgotPassword = await forgotPasswordRepository.GetAsync(x => x.ActivationKey == request.ResetKey,x=>x.Include(x=>x.User));
+                ForgotPassword forgotPassword = await forgotPasswordRepository.GetAsync(x => x.ActivationKey == request.ResetKey, x => x.Include(x => x.User));
 
                 await authBussinessRules.PasswordResetKeyShouldBeExists(forgotPassword);
 
@@ -53,11 +61,33 @@ namespace Application.Features.Auth.Commands.PasswordReset
                 forgotPassword.User.PasswordHash = hash;
 
                 await forgotPasswordRepository.UpdateAsync(forgotPassword);
-               
+
+                await SendPasswordChangeMail(forgotPassword.User);
+
                 return Unit.Value;
 
 
             }
+            private async Task SendPasswordChangeMail(User siteUser)
+            {
+                List<MailboxAddress> mailboxAddresses = new List<MailboxAddress>();
+                mailboxAddresses.Add(new MailboxAddress(Encoding.UTF8, $"{siteUser.FirstName} {siteUser.LastName}", siteUser.Email));
+
+                var info = httpContextAccessor.HttpContext.Request.Headers["User-Agent"].ToString();
+
+                Mail mail = new()
+                {
+                    Subject = "Password Changed",
+                    ToList = mailboxAddresses,
+                    HtmlBody = $"Hi {siteUser.FirstName} {siteUser.LastName} \n" +
+                    $"Your password is changed  at, \n" +
+                    $"{info}"
+                };
+
+                await mailService.SendEmailAsync(mail);
+
+            }
+
         }
     }
 }
