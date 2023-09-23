6f220cb42b5ac1d51c7c94fcec3c920a73a2db6f
Esquetta <esquetta@gmail.com>
2023-09-23 18:45:33 +0300

    Facebook login dto and IExternalService created.

---
+++ Infrastructure/Infrastructure/Dtos/GoogleLoginResponse.cs
@@ -0,0 +1,11 @@
+﻿using Core.Security.Entities;
+using Core.Security.JWT;
+
+namespace Infrastructure.Dtos
+{
+    public class GoogleLoginResponse
+    {
+        public AccessToken? AccessToken { get; set; }
+        public RefreshToken? RefreshToken { get; set; }
+    }
+}
---
+++ Infrastructure/Infrastructure/IExternalAuthService.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using Infrastructure.Dtos;
+using static Google.Apis.Auth.GoogleJsonWebSignature;
+
+namespace Infrastructure
+{
+    public interface IExternalAuthService
+    {
+        Task<GoogleLoginResponse> CreateUserExternalAsync(SiteUser user, string email, string name, string surname, string picture, string ipAdress);
+        Task<Payload> GoogleSignIn(string idToken);
+    }
+}
