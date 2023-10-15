e282a906d19dccbd1211a950a31f2e214edcfbd6
Esquetta <esquetta@gmail.com>
2023-10-15 19:36:38 +0300

    Facebook sign in command validator added.

---
+++ Application/Application/Features/Auth/Commands/FacebookSignIn/FacebookSignInCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Auth.Commands.FacebookSignIn
+{
+    public class FacebookSignInCommandValidator:AbstractValidator<FacebookSignInCommand>
+    {
+        public FacebookSignInCommandValidator()
+        {
+            RuleFor(x=>x.Token).NotEmpty();
+        }
+    }
+}
