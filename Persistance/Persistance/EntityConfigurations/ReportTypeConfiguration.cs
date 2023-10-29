10276342b09df08d1eaaaaabc2d7cdcdc1609e51
Esquetta <esquetta@gmail.com>
2023-10-29 18:48:24 +0300

    Report Types entity seeds added.

--- Persistance/Persistance/EntityConfigurations/ReportTypeConfiguration.cs
+++ Persistance/Persistance/EntityConfigurations/ReportTypeConfiguration.cs
@@ -8,7 +8,16 @@ namespace Persistance.EntityConfigurations
     {
         public void Configure(EntityTypeBuilder<ReportType> builder)
         {
-            
+
+            ReportType[] reportTypes = new[] { new ReportType(1, "Negative Attitude", "Putting people down or being negative about the website or article"),
+                                               new ReportType(2, "Verbal Abuse", "Including, but not limited to language that is unlawful, harmful, threatening, abusive, harassing, defamatory, vulgar, obscene, sexually explicit, or otherwise objectionable."),
+                                            new ReportType(3,"Hate Speech","Racism, sexism, homophobia, etc."),
+                                            new ReportType(4,"Harassment","Unwanted behavior, physical or verbal (or even suggested), that makes a reasonable person feel uncomfortable, humiliated, or mentally distressed."),
+                                            new ReportType(5,"Spam","Stupid pointless annoying articles")
+
+            };
+
+            builder.HasData(reportTypes);
         }
     }
 }
