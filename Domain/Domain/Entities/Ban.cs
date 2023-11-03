c4e11065e580fb8a8b05a1ad44ac41c1fc368ed5
Esquetta <esquetta@gmail.com>
2023-11-03 22:26:24 +0300

    Entity update

--- Domain/Domain/Entities/Ban.cs
+++ Domain/Domain/Entities/Ban.cs
@@ -5,23 +5,20 @@ namespace Domain.Entities
     public class Ban : Entity<Guid>
     {
         public Guid ReportID { get; set; }
-        public int UserID { get; set; }
         public bool IsPerma { get; set; }
         public DateTime BanStartDate { get; set; }
         public DateTime BanEndDate { get; set; }
         public string? BanDetail { get; set; }
 
         public virtual Report Report { get; set; }
-        public virtual SiteUser User { get; set; }
 
         public Ban()
         {
 
         }
-        public Ban(Guid id, int userID, Guid repotId, bool isPerma, DateTime banStartDate, DateTime BanEndDate, string? bandDetail) : this()
+        public Ban(Guid id,Guid repotId, bool isPerma, DateTime banStartDate, DateTime BanEndDate, string? bandDetail) : this()
         {
             this.Id = id;
-            this.UserID = userID;
             this.ReportID = repotId;
             this.IsPerma = isPerma;
             this.BanStartDate = banStartDate;
