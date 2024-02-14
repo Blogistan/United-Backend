fab60b330c283d1013476c3b7046e4000e7babd2
esquetta <esquetta@gmail.com>
2024-02-14 21:08:59 +0300

    Command validator.

---
+++ Application/Application/Features/Auth/Commands/GithubSignIn/GithubSignInCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Auth.Commands.GithubSignIn
+{
+    public class GithubSignInCommandValidator:AbstractValidator<GithubSignInCommand>
+    {
+        public GithubSignInCommandValidator()
+        {
+            RuleFor(x=>x.Token).NotEmpty();
+        }
+    }
+}
