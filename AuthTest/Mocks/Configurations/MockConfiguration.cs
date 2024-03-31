d732212c27b3a6ebaf760cb16b4e3ec561d7f809
esquetta <esquetta@gmail.com>
2024-03-31 14:24:32 +0300

    Auth test projects created. Moq config added.

---
+++ AuthTest/Mocks/Configurations/MockConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace AuthTest.Mocks.Configurations
+{
+    public class MockConfiguration
+    {
+        public static IConfiguration GetMockConfiguration()
+        {
+            var mockConfiguration = new Dictionary<string, string>
+            {
+                { "TokenOptions:Audience", "United-Backend" },
+                { "TokenOptions:Issuer", "UZUMYMWKWKAPODSasda564213" },
+                { "TokenOptions:AccessTokenExpiration", "10" },
+                { "TokenOptions:SecurityKey", "yr.y(&6fYcv5u;JDFh+RK36N5HjS(Ur,t(KdX#E+hTOx!2J%gbqM;6AL@G5(#+53Pip$Y$xo(k2)2HnrW%rF1mVkHvN8O9wD.shl" },
+                { "TokenOptions:RefreshTokenExpiration", "1440" },
+                { "TokenOptions:RefreshTokenTTL", "180" },
+                { "MailSettings:Server", "127.0.0.1" },
+
+            };
+
+            IConfigurationBuilder configurationBuilder = new ConfigurationBuilder().AddInMemoryCollection(mockConfiguration);
+            return configurationBuilder.Build();
+        }
+    }
+}
