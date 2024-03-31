8e602e68ee56bebda58006c57fc0208c4cd689a7
esquetta <esquetta@gmail.com>
2024-03-31 15:48:27 +0300

    Fakes datas created.

---
+++ AuthTest/Mocks/FakeDatas/RefreshTokenFakeData.cs
@@ -0,0 +1,13 @@
+﻿using Core.Security.Entities;
+using Core.Test.Application.FakeData;
+
+namespace AuthTest.Mocks.FakeDatas
+{
+    public class RefreshTokenFakeData : BaseFakeData<RefreshToken, int>
+    {
+        public override List<RefreshToken> CreateFakeData()
+        {
+            return new List<RefreshToken> { new() { UserId = UserFakeData.Ids[0], Token = "abc" } };
+        }
+    }
+}
---
+++ AuthTest/Mocks/FakeDatas/UserFakeData.cs
@@ -0,0 +1,44 @@
+﻿using Core.Security.Entities;
+using Core.Security.Hashing;
+using Core.Test.Application.FakeData;
+
+namespace AuthTest.Mocks.FakeDatas
+{
+    public class UserFakeData : BaseFakeData<User, int>
+    {
+        public static int[] Ids = new int[] { 234, 123, 12 };
+        public override List<User> CreateFakeData()
+        {
+            HashingHelper.CreatePasswordHash("123456", out byte[] passwordHash, out byte[] passwordSalt);
+            List<User> data = new List<User>
+            {
+                new User
+            {
+                Id = Ids[0],
+                Email = "example@kodlama.io",
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
+                CreatedDate = DateTime.Now,
+            },
+            new User
+            {
+                Id = Ids[1],
+                Email = "example2@kodlama.io",
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
+                CreatedDate = DateTime.Now,
+            },
+            new User
+            {
+                Id = Ids[2],
+                Email = "example3@kodlama.io",
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
+                CreatedDate = DateTime.Now,
+            }
+            };
+            return data;
+
+        }
+    }
+}
