6f34bedf0a5a3eb1c3098a1b243176e781e1fe8a
esquetta <esquetta@gmail.com>
2024-04-05 20:51:44 +0300

    User Business rules added.

---
+++ Application/Application/Features/SiteUsers/Rules/UserBusinessRules.cs
@@ -0,0 +1,51 @@
+﻿using Application.Features.Auth.Constants;
+using Application.Features.Auth.Rules;
+using Application.Services.Repositories;
+using Core.Application.Rules;
+using Core.CrossCuttingConcerns.Exceptions.Types;
+using Core.Security.Entities;
+using Core.Security.Hashing;
+
+namespace Application.Features.SiteUsers.Rules
+{
+    public class UserBusinessRules : BaseBusinessRules
+    {
+        private readonly ISiteUserRepository siteUserRepository;
+        public UserBusinessRules(ISiteUserRepository siteUserRepository)
+        {
+            this.siteUserRepository = siteUserRepository;
+        }
+        public async Task UserShouldBeExistsWhenSelected(User? user)
+        {
+            if (user == null)
+                throw new BusinessException(AuthBusinessMessage.UserNotFound);
+        }
+
+        public async Task UserIdShouldBeExistsWhenSelected(int id)
+        {
+            bool doesExist = await siteUserRepository.AnyAsync(predicate: u => u.Id == id);
+            if (doesExist)
+                throw new BusinessException(AuthBusinessMessage.UserNotFound);
+        }
+
+        public async Task UserPasswordShouldBeMatched(User user, string password)
+        {
+            if (!HashingHelper.VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+                throw new BusinessException(AuthBusinessMessage.InvlaidPassword);
+        }
+
+        public async Task UserEmailShouldNotExistsWhenInsert(string email)
+        {
+            bool doesExists = await siteUserRepository.AnyAsync(predicate: u => u.Email == email);
+            if (doesExists)
+                throw new BusinessException(AuthBusinessMessage.UserEmailAlreadyExists);
+        }
+
+        public async Task UserEmailShouldNotExistsWhenUpdate(int id, string email)
+        {
+            bool doesExists = await siteUserRepository.AnyAsync(predicate: u => u.Id != id && u.Email == email);
+            if (doesExists)
+                throw new BusinessException(AuthBusinessMessage.UserNotFound);
+        }
+    }
+}
