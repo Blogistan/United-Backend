72ae38ed2ab0a6b0f2962f3e56ee0c431814407f
esquetta <esquetta@gmail.com>
2024-04-14 15:15:39 +0300

    Query tests added.

---
+++ AuthTest/Features/Users/Queries/GetById/GetByIdSiteUserQuery.cs
@@ -0,0 +1,37 @@
+﻿using Application.Features.SiteUsers.Queries.GetById;
+using AuthTest.Mocks.FakeDatas;
+using AuthTest.Mocks.Repositories;
+using Core.CrossCuttingConcerns.Exceptions.Types;
+using static Application.Features.SiteUsers.Queries.GetById.GetByIdSiteUserQuery;
+
+namespace AuthTest.Features.Users.Queries.GetById
+{
+    public class GetByIdSiteUserQueryTest : UserMockRepository
+    {
+        private readonly GetByIdSiteUserQuery getByIdSiteUserQuery;
+        private readonly GetByIdSiteUserQueryHandler getByIdSiteUserQueryHandler;
+        public GetByIdSiteUserQueryTest(SiteUserFakeData siteUserFakeData, GetByIdSiteUserQuery getByIdSiteUserQuery, GetByIdSiteUserQueryHandler getByIdSiteUserQueryHandler) : base(siteUserFakeData)
+        {
+            this.getByIdSiteUserQuery = getByIdSiteUserQuery;
+            this.getByIdSiteUserQueryHandler = new GetByIdSiteUserQueryHandler(MockRepository.Object, Mapper);
+        }
+        [Fact]
+        public async Task GetByIdUserShouldSuccessfully()
+        {
+            getByIdSiteUserQuery.Id = SiteUserFakeData.Ids[0];
+
+            GetByIdSiteUserQueryResponse response = await getByIdSiteUserQueryHandler.Handle(getByIdSiteUserQuery, CancellationToken.None);
+
+            Assert.Equal("test@mailinator.com", response.SiteUserListViewDto.Email);
+        }
+        [Fact]
+        public async Task UserIdNotExistsShouldReturnError()
+        {
+            getByIdSiteUserQuery.Id = SiteUserFakeData.Ids[0];
+
+            async Task Action() => await getByIdSiteUserQueryHandler.Handle(getByIdSiteUserQuery, CancellationToken.None);
+
+            await Assert.ThrowsAsync<BusinessException>(Action);
+        }
+    }
+}
---
+++ AuthTest/Features/Users/Queries/GetList/GetListSiteUserQueryTest.cs
@@ -0,0 +1,30 @@
+﻿using Application.Features.SiteUsers.Queries.GetList;
+using AuthTest.Mocks.FakeDatas;
+using AuthTest.Mocks.Repositories;
+using Core.Application.Requests;
+using static Application.Features.SiteUsers.Queries.GetList.GetListSiteUserQuery;
+
+namespace AuthTest.Features.Users.Queries.GetList
+{
+    public class GetListSiteUserQueryTest : UserMockRepository
+    {
+        private readonly GetListSiteUserQuery getListSiteUserQuery;
+        private readonly GetListSiteUserQueryHandler getListSiteUserQueryHandler;
+
+        public GetListSiteUserQueryTest(SiteUserFakeData siteUserFakeData, GetListSiteUserQuery getListSiteUserQuery, GetListSiteUserQueryHandler getListSiteUserQueryHandler) : base(siteUserFakeData)
+        {
+            this.getListSiteUserQuery = getListSiteUserQuery;
+            this.getListSiteUserQueryHandler = getListSiteUserQueryHandler;
+        }
+        [Fact]
+        public async Task GetAllUsersShouldSuccessfuly()
+        {
+            getListSiteUserQuery.PageRequest = new PageRequest { Page = 0, PageSize = 3 };
+
+            GetListSiteUserQueryResponse response = await getListSiteUserQueryHandler.Handle(getListSiteUserQuery, CancellationToken.None);
+
+            Assert.Equal(3, response.Items.Count);
+        }
+
+    }
+}
