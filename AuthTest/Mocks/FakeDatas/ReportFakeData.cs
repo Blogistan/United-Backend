ac0ca042e1413fc967a0b875cecfa548c401377b
esquetta <esquetta@gmail.com>
2024-05-15 21:41:54 +0300

    ReportFake data added.

---
+++ AuthTest/Mocks/FakeDatas/ReportFakeData.cs
@@ -0,0 +1,18 @@
+﻿using Core.Test.Application.FakeData;
+using Domain.Entities;
+
+namespace AuthTest.Mocks.FakeDatas
+{
+    public class ReportFakeData : BaseFakeData<Report, Guid>
+    {
+        public override List<Report> CreateFakeData()
+        {
+            return new List<Report>
+            {
+                new(){Id=Guid.NewGuid(),UserID=1,ReportTypeID=1,ReportDescription="TEST REPORT"},
+                new(){Id=Guid.NewGuid(),UserID=1,ReportTypeID=2,ReportDescription="TEST REPORT"},
+                new(){Id=Guid.NewGuid(),UserID=1,ReportTypeID=3,ReportDescription="TEST REPORT"}
+            };
+        }
+    }
+}
