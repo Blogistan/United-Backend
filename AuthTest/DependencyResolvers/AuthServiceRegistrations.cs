1cb4b1ba0426329e3d51203adb9bffae6d5fe5e1
esquetta <esquetta@gmail.com>
2024-05-16 21:21:19 +0300

    Perma fake data added to service registrations.

--- AuthTest/DependencyResolvers/AuthServiceRegistrations.cs
+++ AuthTest/DependencyResolvers/AuthServiceRegistrations.cs
@@ -10,6 +10,7 @@ namespace AuthTest.DependencyResolvers
         {
             services.AddTransient<SiteUserFakeData>();
             services.AddTransient<OperationClaimFakeData>();
+            services.AddTransient<BanFakeData>();
             services.AddTransient<UserOperationClaimFakeData>();
             services.AddTransient<RefreshTokenFakeData>();
             services.AddTransient<LoginCommand>();
