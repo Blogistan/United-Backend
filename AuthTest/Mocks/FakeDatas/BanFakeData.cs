9bc05329dc1e3bd873ed8448594bfbaddf2832b6
esquetta <esquetta@gmail.com>
2024-05-16 21:03:37 +0300

    Ban fake data created.

---
+++ AuthTest/Mocks/FakeDatas/BanFakeData.cs
@@ -0,0 +1,17 @@
+﻿using Core.Test.Application.FakeData;
+using Domain.Entities;
+
+namespace AuthTest.Mocks.FakeDatas
+{
+    public class BanFakeData : BaseFakeData<Ban, Guid>
+    {
+        public override List<Ban> CreateFakeData()
+        {
+            return new List<Ban>
+            {
+                new(){Id=Guid.NewGuid(),UserID=123,IsPerma=true,BanDetail="Test ban",BanStartDate=DateTime.UtcNow,BanEndDate=DateTime.UtcNow.AddYears(1)},
+                new(){Id=Guid.NewGuid(),UserID=234,IsPerma=true,BanDetail="Test ban",BanStartDate=DateTime.UtcNow,BanEndDate=DateTime.UtcNow.AddYears(1)}
+            };
+        }
+    }
+}
