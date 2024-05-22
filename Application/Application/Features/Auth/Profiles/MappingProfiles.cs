f8bc9b666f676f1da0e918944d13a830ea63de39
esquetta <esquetta@gmail.com>
2024-05-22 15:23:06 +0300

    Mappings added for register dtos.

--- Application/Application/Features/Auth/Profiles/MappingProfiles.cs
+++ Application/Application/Features/Auth/Profiles/MappingProfiles.cs
@@ -1,7 +1,9 @@
 ﻿using Application.Features.Auth.Commands.Revoke;
+using Application.Features.Auth.Dtos;
 using AutoMapper;
 using Core.Application.Dtos;
 using Core.Security.Entities;
+using Core.Security.JWT;
 using Domain.Entities;
 
 namespace Application.Features.Auth.Profiles
@@ -12,7 +14,11 @@ namespace Application.Features.Auth.Profiles
         {
             CreateMap<SiteUser, UserForRegisterDto>().ReverseMap();
 
+            CreateMap<AccessToken, AccessTokenDto>().ReverseMap();
+            CreateMap<RefreshToken, RefreshTokenDto>().ReverseMap();
             CreateMap<RevokedResponse, RefreshToken>().ReverseMap();
+
+           
         }
     }
 }
