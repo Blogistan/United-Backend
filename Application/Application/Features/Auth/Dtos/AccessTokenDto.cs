f7d1b630c4df7b6de28b02d73e694a571920d89c
esquetta <esquetta@gmail.com>
2024-05-22 15:22:49 +0300

    Register Dtos created.

---
+++ Application/Application/Features/Auth/Dtos/AccessTokenDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Features.Auth.Dtos
+{
+    public class AccessTokenDto
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
---
+++ Application/Application/Features/Auth/Dtos/RefreshTokenDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Application.Features.Auth.Dtos
+{
+    public class RefreshTokenDto
+    {
+        public int UserId { get; set; }
+        public string Token { get; set; }
+        public DateTime Expires { get; set; }
+        public string CreatedByIp { get; set; }
+        public DateTime? Revoked { get; set; }
+        public string? RevokedByIp { get; set; }
+        public string? ReplacedByToken { get; set; }
+
+        public string? ReasonRevoked { get; set; }
+    }
+}
