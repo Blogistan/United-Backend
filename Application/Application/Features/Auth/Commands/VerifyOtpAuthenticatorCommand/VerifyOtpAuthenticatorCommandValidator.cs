0239f5694dfcd234c7a15f9309d20abf513cf5f9
esquetta <esquetta@gmail.com>
2024-06-01 15:55:34 +0300

    VerifyEmailAuth validator added.

---
+++ Application/Application/Features/Auth/Commands/VerifyOtpAuthenticatorCommand/VerifyOtpAuthenticatorCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Auth.Commands.VerifyOtpAuthenticatorCommand
+{
+    public class VerifyOtpAuthenticatorCommandValidator : AbstractValidator<VerifyOtpAuthenticatorCommand>
+    {
+        public VerifyOtpAuthenticatorCommandValidator()
+        {
+            RuleFor(x => x.UserId).NotEmpty();
+            RuleFor(x => x.OtpCode).NotEmpty();
+        }
+    }
+}
