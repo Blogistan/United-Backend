5dfd96b4f6aa21deb7e2cbfe8089ef47c53e90a3
esquetta <esquetta@gmail.com>
2024-06-01 14:55:23 +0300

    Otp Authenticator fake data added.

---
+++ AuthTest/Mocks/FakeDatas/OtpAuthenticatorFakeData.cs
@@ -0,0 +1,21 @@
+﻿using Core.Security.Entities;
+using Core.Security.Hashing;
+using Core.Test.Application.FakeData;
+
+namespace AuthTest.Mocks.FakeDatas
+{
+    public class OtpAuthenticatorFakeData : BaseFakeData<OtpAuthenticator, int>
+    {
+        
+        public override List<OtpAuthenticator> CreateFakeData()
+        {
+            HashingHelper.CreatePasswordHash("123456", out byte[] passwordHash, out byte[] passwordSalt);
+            return new List<OtpAuthenticator>()
+            {
+                new(){Id=1,UserId=234,IsVerified=false,SecretKey=passwordHash},
+                new(){Id=2,UserId=123,IsVerified=false,SecretKey=passwordHash},
+                new(){Id=3,UserId=12,IsVerified=false,SecretKey=passwordHash},
+            };
+        }
+    }
+}
