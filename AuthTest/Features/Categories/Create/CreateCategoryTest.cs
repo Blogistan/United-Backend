8c9734130125ca022c01f1f182ffa755065ce2b3
esquetta <esquetta@gmail.com>
2024-06-05 17:36:19 +0300

    Create,Delete,Update category tests added.

---
+++ AuthTest/Features/Categories/Create/CreateCategoryTest.cs
@@ -0,0 +1,47 @@
+﻿using Application.Features.Categories.Commands.CreateCategory;
+using Application.Features.Categories.Rules;
+using Application.Services.Repositories;
+using AuthTest.Mocks.FakeDatas;
+using AuthTest.Mocks.Repositories;
+using Core.CrossCuttingConcerns.Exceptions.Types;
+using FluentValidation.TestHelper;
+using static Application.Features.Categories.Commands.CreateCategory.CreateCategoryCommand;
+
+namespace AuthTest.Features.Categories.Create
+{
+    public class CreateCategoryTest : CategoryMockRepository, IClassFixture<Startup>
+    {
+        private readonly CategoryFakeData categoryFakeData;
+        private readonly CreateCategoryCommand createCategoryCommand;
+        private readonly CreateCategoryCommandHandler createCategoryCommandHandler;
+        private readonly CreateCategoryValidator validationRules;
+        public CreateCategoryTest(CategoryFakeData fakeData) : base(fakeData)
+        {
+
+            ICategoryRepository categoryRepository = MockRepository.Object;
+            CategoryBusinessRules categoryBusinessRules = BusinessRules;
+
+            this.categoryFakeData = fakeData;
+            this.createCategoryCommand = new CreateCategoryCommand();
+            this.validationRules = new CreateCategoryValidator();
+            this.createCategoryCommandHandler = new CreateCategoryCommandHandler(categoryRepository, categoryBusinessRules);
+        }
+        [Fact]
+        public async Task ThrowExceptionIfCategoryNameEmpty()
+        {
+            TestValidationResult<CreateCategoryCommand> testValidationResult = validationRules.TestValidate(createCategoryCommand);
+            testValidationResult.ShouldHaveValidationErrorFor(x => x.CategoryName);
+        }
+        [Fact]
+        public async Task ThowExceptionIfCategoryAlreadyExists()
+        {
+            createCategoryCommand.CategoryName = "TestCat1";
+
+            await Assert.ThrowsAsync<BusinessException>(async () =>
+            {
+                await createCategoryCommandHandler.Handle(createCategoryCommand, CancellationToken.None);
+            });
+        }
+
+    }
+}
---
+++ AuthTest/Mocks/Repositories/CategoryMockRepository.cs
@@ -0,0 +1,17 @@
+﻿using Application.Features.Categories.Profiles;
+using Application.Features.Categories.Rules;
+using Application.Services.Repositories;
+using AuthTest.Mocks.FakeDatas;
+using Core.Test.Application.Repositories;
+using Domain.Entities;
+using MediatR;
+
+namespace AuthTest.Mocks.Repositories
+{
+    public class CategoryMockRepository : BaseMockRepository<ICategoryRepository, Category, int, MappingProfiles, CategoryBusinessRules, CategoryFakeData, IMediator>
+    {
+        public CategoryMockRepository(CategoryFakeData fakeData) : base(fakeData)
+        {
+        }
+    }
+}
