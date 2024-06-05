eb6adf5853e4c146ffc40d1e545fd8640371e754
esquetta <esquetta@gmail.com>
2024-06-05 16:51:30 +0300

    CategoryFake data added.

---
+++ AuthTest/Mocks/FakeDatas/CategoryFakeData.cs
@@ -0,0 +1,18 @@
+﻿using Core.Test.Application.FakeData;
+using Domain.Entities;
+
+namespace AuthTest.Mocks.FakeDatas
+{
+    public class CategoryFakeData : BaseFakeData<Category, int>
+    {
+        public override List<Category> CreateFakeData()
+        {
+            return new List<Category>
+            {
+                new(){Id=1,CategoryName="TestCat1"},
+                new(){Id=2,CategoryName="TestCat2"},
+                new(){Id=3,CategoryName="TestCat3"},
+            };
+        }
+    }
+}
