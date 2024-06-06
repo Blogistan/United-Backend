41cb8b904f9152d3b85048accfdeb2be11c5625e
esquetta <esquetta@gmail.com>
2024-06-06 16:53:42 +0300

    Code refactoring.

---
+++ Application/Application/Features/Categories/Commands/DeleteRangeCategory/DeleteRangeCategoryCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Categories.Commands.DeleteRangeCategory
+{
+    public class DeleteRangeCategoryCommandValidator:AbstractValidator<DeleteRangeCategoryCommand>
+    {
+        public DeleteRangeCategoryCommandValidator()
+        {
+            RuleFor(x=>x.Permanent).NotEmpty();
+            RuleFor(x=>x.DeleteRangeCategoryDtos).NotEmpty();
+        }
+    }
+}
