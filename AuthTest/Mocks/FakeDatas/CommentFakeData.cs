7dfc0dcfbfc37ffe083cc50d948f841aa3938065
esquetta <esquetta@gmail.com>
2024-06-08 16:09:45 +0300

    Comment fake data added.

---
+++ AuthTest/Mocks/FakeDatas/CommentFakeData.cs
@@ -0,0 +1,19 @@
+﻿using Core.Test.Application.FakeData;
+using Domain.Entities;
+
+namespace AuthTest.Mocks.FakeDatas
+{
+    public class CommentFakeData : BaseFakeData<Comment, int>
+    {
+        public override List<Comment> CreateFakeData()
+        {
+            return new List<Comment>
+            {
+                new(){Id=1,BlogId=1,CommentContent="TEST COMMENT",UserId=1},
+                new(){Id=2,BlogId=1,CommentContent="TEST COMMENT1",UserId=2},
+                new(){Id=3,BlogId=1,CommentContent="TEST COMMENT2",UserId=3},
+
+            };
+        }
+    }
+}
