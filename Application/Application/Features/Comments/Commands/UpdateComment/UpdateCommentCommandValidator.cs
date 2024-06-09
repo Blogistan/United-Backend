4101acfbf614f88f62e97fd05eabb6df10d84e56
esquetta <esquetta@gmail.com>
2024-06-09 16:41:49 +0300

    Code refactoring.

---
+++ Application/Application/Features/Comments/Commands/UpdateComment/UpdateCommentCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Comments.Commands.UpdateComment
+{
+    public class UpdateCommentCommandValidator:AbstractValidator<UpdateCommentCommand>
+    {
+        public UpdateCommentCommandValidator()
+        {
+            RuleFor(x => x.CommentContent).NotNull();
+            RuleFor(x => x.Id).NotNull();
+        }
+    }
+}
