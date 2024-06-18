6d4fe47b2e6db901f7c87aecabe7995158dda0dd
esquetta <esquetta@gmail.com>
2024-06-18 15:30:18 +0300

    Content Fake data created

---
+++ AuthTest/Mocks/FakeDatas/ContentFakeData.cs
@@ -0,0 +1,20 @@
+﻿using Core.Test.Application.FakeData;
+using Domain.Entities;
+
+namespace AuthTest.Mocks.FakeDatas
+{
+    public class ContentFakeData : BaseFakeData<Content, int>
+    {
+        public override List<Content> CreateFakeData()
+        {
+            return new List<Content>
+            {
+                new(){Id=1,Title="Test Content",ContentPragraph="ASELOGJSDIOGHSDUPIGHIUAPQGBHUYIAQPHGUŞKASDBHNGŞOUISDN",CreateUser=1,CreatedDate=DateTime.UtcNow,DeletedDate=null},
+                new(){Id=2,Title="Test Content2",ContentPragraph="asd",CreateUser=1,CreatedDate=DateTime.UtcNow,DeletedDate=null},
+                new(){Id=3,Title="Test Content3",ContentPragraph="dgbfdhfghgfhgf",CreateUser=1,CreatedDate=DateTime.UtcNow,DeletedDate=null},
+
+
+            };
+        }
+    }
+}
