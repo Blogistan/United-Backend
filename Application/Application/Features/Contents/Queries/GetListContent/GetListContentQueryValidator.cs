8eb83a2c77107da5e574916b31a22fe4378465d8
esquetta <esquetta@gmail.com>
2024-06-21 15:34:58 +0300

    Query validator added.

---
+++ Application/Application/Features/Contents/Queries/GetListContent/GetListContentQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Contents.Queries.GetListContent
+{
+    public class GetListContentQueryValidator : AbstractValidator<GetListContentQuery>
+    {
+        public GetListContentQueryValidator()
+        {
+            RuleFor(x => x.PageRequest).NotEmpty();
+        }
+    }
+}
