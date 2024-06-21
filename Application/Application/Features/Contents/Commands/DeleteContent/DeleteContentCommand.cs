2daa0be4da267a6467c8bdcdf4d2039a71ac2c66
esquetta <esquetta@gmail.com>
2024-06-21 15:17:42 +0300

    Content business rules added.

--- Application/Application/Features/Contents/Commands/DeleteContent/DeleteContentCommand.cs
+++ Application/Application/Features/Contents/Commands/DeleteContent/DeleteContentCommand.cs
@@ -1,11 +1,12 @@
-﻿using Application.Services.Repositories;
+﻿using Application.Features.Contents.Rules;
+using Application.Services.Repositories;
 using Core.Application.Pipelines.Authorization;
 using Domain.Entities;
 using MediatR;
 
 namespace Application.Features.Contents.Commands.DeleteContent
 {
-    public class DeleteContentCommand : IRequest<DeleteContentCommandResponse>,ISecuredRequest
+    public class DeleteContentCommand : IRequest<DeleteContentCommandResponse>, ISecuredRequest
     {
         public int Id { get; set; }
         string[] ISecuredRequest.Roles => new string[] { "Admin", "Moderator", "Blogger", "User" };
@@ -13,17 +14,19 @@ namespace Application.Features.Contents.Commands.DeleteContent
         public class DeleteContentCommandHandler : IRequestHandler<DeleteContentCommand, DeleteContentCommandResponse>
         {
             private readonly IContentRepository contentRepository;
-            public DeleteContentCommandHandler(IContentRepository contentRepository)
+            private readonly ContentBusinessRules contentBusinessRules;
+            public DeleteContentCommandHandler(IContentRepository contentRepository, ContentBusinessRules contentBusinessRules)
             {
                 this.contentRepository = contentRepository;
+                this.contentBusinessRules = contentBusinessRules;
             }
 
             public async Task<DeleteContentCommandResponse> Handle(DeleteContentCommand request, CancellationToken cancellationToken)
             {
-                var content = await contentRepository.GetAsync(x => x.Id == request.Id);
+                var content = await contentBusinessRules.ContentCheckById(request.Id);
 
                 Content deletedContent = await contentRepository.DeleteAsync(content);
-             
+
                 return new DeleteContentCommandResponse
                 {
 
