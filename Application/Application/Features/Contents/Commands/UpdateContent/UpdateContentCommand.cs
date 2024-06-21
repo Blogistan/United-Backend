c32e99ea0383287a9e4a4c510094f355f3a94355
esquetta <esquetta@gmail.com>
2024-06-21 15:23:16 +0300

    Command updated.

--- Application/Application/Features/Contents/Commands/UpdateContent/UpdateContentCommand.cs
+++ Application/Application/Features/Contents/Commands/UpdateContent/UpdateContentCommand.cs
@@ -1,11 +1,12 @@
-﻿using Application.Services.Repositories;
+﻿using Application.Features.Contents.Rules;
+using Application.Services.Repositories;
 using Core.Application.Pipelines.Authorization;
 using Domain.Entities;
 using MediatR;
 
 namespace Application.Features.Contents.Commands.UpdateContent
 {
-    public class UpdateContentCommand : IRequest<UpdateContentCommandResponse>,ISecuredRequest
+    public class UpdateContentCommand : IRequest<UpdateContentCommandResponse>, ISecuredRequest
     {
         public int Id { get; set; }
         public string Title { get; set; }
@@ -18,20 +19,23 @@ namespace Application.Features.Contents.Commands.UpdateContent
         public class UpdateContentCommandHandler : IRequestHandler<UpdateContentCommand, UpdateContentCommandResponse>
         {
             private readonly IContentRepository contentRepository;
-            public UpdateContentCommandHandler(IContentRepository contentRepository)
+            private readonly ContentBusinessRules contentBusinessRules;
+            public UpdateContentCommandHandler(IContentRepository contentRepository, ContentBusinessRules contentBusinessRules)
             {
                 this.contentRepository = contentRepository;
+                this.contentBusinessRules = contentBusinessRules;
             }
 
             public async Task<UpdateContentCommandResponse> Handle(UpdateContentCommand request, CancellationToken cancellationToken)
             {
-                Content content = new()
-                {
-                    Id = request.Id,
-                    Title = request.Title,
-                    ContentImageUrl = request.ContentImageUrl,
-                    ContentPragraph = request.ContentPragraph
-                };
+                var content = await contentBusinessRules.ContentCheckById(request.Id);
+
+                content.Id = request.Id;
+                content.Title = request.Title;
+                content.ContentImageUrl = request.ContentImageUrl;
+                content.ContentPragraph = request.ContentPragraph;
+
+
 
                 Content updatedContent = await contentRepository.UpdateAsync(content);
 
