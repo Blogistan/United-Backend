5826ee0ac4d0f10fb802e25c020ae02c4f44feea
esquetta <esquetta@gmail.com>
2024-06-24 16:49:38 +0300

    Query validator added.

---
+++ Application/Application/Features/ReportTypes/Queries/GetListReportTypes/GetListReportTypeQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Features.ReportTypes.Queries.GetListReportTypes
+{
+    public class GetListReportTypeQueryValidator : AbstractValidator<GetListReportTypeQuery>
+    {
+        public GetListReportTypeQueryValidator()
+        {
+            RuleFor(x => x.PageRequest).NotEmpty();
+        }
+    }
+}
