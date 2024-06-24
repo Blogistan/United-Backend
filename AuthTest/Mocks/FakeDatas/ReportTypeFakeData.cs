1886b1b192bd1d959791ea012a450ef2d5a07ec1
esquetta <esquetta@gmail.com>
2024-06-24 15:05:22 +0300

    ReportType fake data created.

---
+++ AuthTest/Mocks/FakeDatas/ReportTypeFakeData.cs
@@ -0,0 +1,18 @@
+﻿using Core.Test.Application.FakeData;
+using Domain.Entities;
+
+namespace AuthTest.Mocks.FakeDatas
+{
+    public class ReportTypeFakeData : BaseFakeData<ReportType, int>
+    {
+        public override List<ReportType> CreateFakeData()
+        {
+            return
+            [
+                new(){Id=1,ReportTypeName="Rahatsız Edici Söylem"},
+                new(){Id=2,ReportTypeName="Aşağılama"},
+                new(){Id=3,ReportTypeName="Taciz"},
+            ];
+        }
+    }
+}
