42e5d9252b425009fb2f763ba88f24798b13f90a
esquetta <esquetta@gmail.com>
2024-06-26 15:42:00 +0300

    Blog fake data added.

---
+++ AuthTest/Mocks/FakeDatas/BlogFakeData.cs
@@ -0,0 +1,18 @@
+﻿using Core.Test.Application.FakeData;
+using Domain.Entities;
+
+namespace AuthTest.Mocks.FakeDatas
+{
+    public class BlogFakeData : BaseFakeData<Blog, int>
+    {
+        public override List<Blog> CreateFakeData()
+        {
+            return
+            [
+                new(){Id=1,CategoryId=1,WriterId=1,ContentId=1,Title="Test Blog"},
+                new(){Id=2,CategoryId=2,WriterId=2,ContentId=2,Title="Test Blog2"},
+                new(){Id=3,CategoryId=3,WriterId=3,ContentId=3,Title="Test Blog3"},
+            ];
+        }
+    }
+}
