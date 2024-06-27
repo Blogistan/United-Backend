575f650c63466c305fa091d41103518d270b8ef4
esquetta <esquetta@gmail.com>
2024-06-27 17:31:13 +0300

    Blog mock repo added.

---
+++ AuthTest/Mocks/Repositories/BlogMockRepository.cs
@@ -0,0 +1,17 @@
+﻿using Application.Features.Blogs.Profiles;
+using Application.Features.Blogs.Rules;
+using Application.Services.Repositories;
+using AuthTest.Mocks.FakeDatas;
+using Core.Test.Application.Repositories;
+using Domain.Entities;
+using MediatR;
+
+namespace AuthTest.Mocks.Repositories
+{
+    public class BlogMockRepository : BaseMockRepository<IBlogRepository, Blog, int, MappingProfiles, BlogBusinessRules, BlogFakeData, IMediator>
+    {
+        public BlogMockRepository(BlogFakeData fakeData) : base(fakeData)
+        {
+        }
+    }
+}
