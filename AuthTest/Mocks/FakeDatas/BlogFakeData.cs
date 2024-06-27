b4e737e402db84e810ba12070f1732e58b7efe1c
esquetta <esquetta@gmail.com>
2024-06-27 17:31:23 +0300

    Blog fake data updated for test cases.

--- AuthTest/Mocks/FakeDatas/BlogFakeData.cs
+++ AuthTest/Mocks/FakeDatas/BlogFakeData.cs
@@ -9,9 +9,9 @@ namespace AuthTest.Mocks.FakeDatas
         {
             return
             [
-                new(){Id=1,CategoryId=1,WriterId=1,ContentId=1,Title="Test Blog"},
-                new(){Id=2,CategoryId=2,WriterId=2,ContentId=2,Title="Test Blog2"},
-                new(){Id=3,CategoryId=3,WriterId=3,ContentId=3,Title="Test Blog3"},
+                new(){Id=1,CategoryId=1,WriterId=1,ContentId=1,Title="Test Blog",BannerImageUrl="Img1"},
+                new(){Id=2,CategoryId=2,WriterId=2,ContentId=2,Title="Test Blog2",BannerImageUrl="Img2"},
+                new(){Id=3,CategoryId=3,WriterId=3,ContentId=3,Title="Test Blog3",BannerImageUrl="Img3"},
             ];
         }
     }
