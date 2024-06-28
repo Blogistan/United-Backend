46470b3b6e417b41c1b20f829b6a61d37a500d65
esquetta <esquetta@gmail.com>
2024-06-28 14:44:15 +0300

    DecreaseSuprisedBlogQuery and validator added.

---
+++ Application/Application/Features/Blogs/Queries/DecreaseSuprisedBlog/DecreaseSuprisedBlogQuery.cs
@@ -0,0 +1,41 @@
+﻿using Application.Features.Blogs.Dtos;
+using Application.Features.Blogs.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Blogs.Queries.DecreaseSuprisedBlog
+{
+    public class DecreaseSuprisedBlogQuery : IRequest<BlogListViewDto>
+    {
+        public int BlogId { get; set; }
+
+
+        public class DecreaseSuprisedBlogQueryHandler : IRequestHandler<DecreaseSuprisedBlogQuery, BlogListViewDto>
+        {
+            private readonly IBlogRepository blogRepository;
+            private readonly IMapper mapper;
+            private readonly BlogBusinessRules blogBusinessRules;
+            public DecreaseSuprisedBlogQueryHandler(IBlogRepository blogRepository, IMapper mapper, BlogBusinessRules blogBusinessRules)
+            {
+                this.blogRepository = blogRepository;
+                this.mapper = mapper;
+                this.blogBusinessRules = blogBusinessRules;
+            }
+
+            public async Task<BlogListViewDto> Handle(DecreaseSuprisedBlogQuery request, CancellationToken cancellationToken)
+            {
+                var blog = await blogBusinessRules.BlogCheckById(request.BlogId);
+
+                blog.ReactionSuprisedCount--;
+
+                Blog updatedBlog = await blogRepository.UpdateAsync(blog);
+
+                BlogListViewDto blogListViewDto = mapper.Map<BlogListViewDto>(updatedBlog);
+
+                return blogListViewDto;
+            }
+        }
+    }
+}
---
+++ Application/Application/Features/Blogs/Queries/DecreaseSuprisedBlog/DecreaseSuprisedBlogQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Blogs.Queries.DecreaseSuprisedBlog
+{
+    public class DecreaseSuprisedBlogQueryValidator : AbstractValidator<DecreaseSuprisedBlogQuery>
+    {
+        public DecreaseSuprisedBlogQueryValidator()
+        {
+            RuleFor(x => x.BlogId).NotEmpty();
+        }
+    }
+}
