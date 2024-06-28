632b1f1f4641bb147da009a0e72493ff8370ff97
esquetta <esquetta@gmail.com>
2024-06-28 14:51:30 +0300

    DecreaseTriggerBlogQuery and validator added.

---
+++ Application/Application/Features/Blogs/Queries/DecreaseTriggerBlog/DecreaseTriggerBlogQuery.cs
@@ -0,0 +1,40 @@
+﻿using Application.Features.Blogs.Dtos;
+using Application.Features.Blogs.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Blogs.Queries.DecreaseTriggerBlog
+{
+    public class DecreaseTriggerBlogQuery:IRequest<BlogListViewDto>
+    {
+        public int BlogId { get; set; }
+
+        public class DecreaseTriggerBlogQueryHandler:IRequestHandler<DecreaseTriggerBlogQuery,BlogListViewDto>
+        {
+            private readonly IBlogRepository blogRepository;
+            private readonly IMapper mapper;
+            private readonly BlogBusinessRules blogBusinessRules;
+            public DecreaseTriggerBlogQueryHandler(IBlogRepository blogRepository, IMapper mapper, BlogBusinessRules blogBusinessRules)
+            {
+                this.blogRepository = blogRepository;
+                this.mapper = mapper;
+                this.blogBusinessRules = blogBusinessRules;
+            }
+
+            public async Task<BlogListViewDto> Handle(DecreaseTriggerBlogQuery request, CancellationToken cancellationToken)
+            {
+                var blog = await blogBusinessRules.BlogCheckById(request.BlogId);
+
+                blog.ReactionTriggeredCount--;
+
+                Blog updatedBlog = await blogRepository.UpdateAsync(blog);
+
+                BlogListViewDto blogListViewDto = mapper.Map<BlogListViewDto>(updatedBlog);
+
+                return blogListViewDto;
+            }
+        }
+    }
+}
---
+++ Application/Application/Features/Blogs/Queries/DecreaseTriggerBlog/DecreaseTriggerBlogQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Blogs.Queries.DecreaseTriggerBlog
+{
+    public class DecreaseTriggerBlogQueryHandler : AbstractValidator<DecreaseTriggerBlogQuery>
+    {
+        public DecreaseTriggerBlogQueryHandler()
+        {
+            RuleFor(x => x.BlogId).NotEmpty();
+        }
+    }
+}
