2c1cd010f10fc30dec16f77046749efc8711f32d
esquetta <esquetta@gmail.com>
2024-06-28 14:36:00 +0300

    DecreaseLovelyBLogQuery and validator added.

---
+++ Application/Application/Features/Blogs/Queries/DecreaseLovelyBlog/DecreaseLovelyBLogQuery.cs
@@ -0,0 +1,41 @@
+﻿using Application.Features.Blogs.Dtos;
+using Application.Features.Blogs.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Blogs.Queries.DecreaseLovelyBlog
+{
+    public class DecreaseLovelyBLogQuery:IRequest<BlogListViewDto>
+    {
+        public int BlogId { get; set; }
+
+
+        public class DecreaseLovelyBLogQueryHandler:IRequestHandler<DecreaseLovelyBLogQuery,BlogListViewDto>
+        {
+            private readonly IBlogRepository blogRepository;
+            private readonly IMapper mapper;
+            private readonly BlogBusinessRules blogBusinessRules;
+            public DecreaseLovelyBLogQueryHandler(IBlogRepository blogRepository, IMapper mapper, BlogBusinessRules blogBusinessRules)
+            {
+                this.blogRepository = blogRepository;
+                this.mapper = mapper;
+                this.blogBusinessRules = blogBusinessRules;
+            }
+
+            public async Task<BlogListViewDto> Handle(DecreaseLovelyBLogQuery request, CancellationToken cancellationToken)
+            {
+                var blog = await blogBusinessRules.BlogCheckById(request.BlogId);
+
+                blog.ReactionLovelyCount--;
+
+                Blog updatedBlog = await blogRepository.UpdateAsync(blog);
+
+                BlogListViewDto blogListViewDto = mapper.Map<BlogListViewDto>(updatedBlog);
+
+                return blogListViewDto;
+            }
+        }
+    }
+}
---
+++ Application/Application/Features/Blogs/Queries/DecreaseLovelyBlog/DecreaseLovelyBLogQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Blogs.Queries.DecreaseLovelyBlog
+{
+    public class DecreaseLovelyBLogQueryValidator : AbstractValidator<DecreaseLovelyBLogQuery>
+    {
+        public DecreaseLovelyBLogQueryValidator()
+        {
+            RuleFor(x => x.BlogId).NotEmpty();
+        }
+    }
+}
