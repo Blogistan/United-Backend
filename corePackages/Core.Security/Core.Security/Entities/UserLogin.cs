a7e220f3ea4069c6b2382a106bb853f62bcfe10e
esquetta <esquetta@gmail.com>
2024-07-17 17:41:32 +0300

    UserLogin added.

---
+++ corePackages/Core.Security/Core.Security/Entities/UserLogin.cs
@@ -0,0 +1,32 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Core.Security.Entities
+{
+    public class UserLogin : Entity<int>
+    {
+        public string ProviderName { get; set; }
+        public string ProviderKey { get; set; }
+        public string ProviderDisplayName { get; set; }
+        public int SiteUserId { get; set; }
+        public virtual User User { get; set; } = null!;
+
+        public UserLogin()
+        {
+
+        }
+        public UserLogin(string providerName, string providerKey, string providerDisplayName, int userID)
+        {
+            this.ProviderName = providerName;
+            this.ProviderKey = providerKey;
+            this.ProviderDisplayName = providerDisplayName;
+            this.SiteUserId = userID;
+        }
+        public UserLogin(int id, string providerName, string providerKey, string providerDisplayName, int userID) : base(id)
+        {
+            this.ProviderName = providerName;
+            this.ProviderKey = providerKey;
+            this.ProviderDisplayName = providerDisplayName;
+            this.SiteUserId = userID;
+        }
+    }
+}
