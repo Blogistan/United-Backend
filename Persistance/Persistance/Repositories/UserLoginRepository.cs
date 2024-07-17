3e8c57d10f3a6c1f3d8b49d656d90b5075df6ee0
esquetta <esquetta@gmail.com>
2024-07-17 17:42:38 +0300

    UserLogin repository added.

---
+++ Persistance/Persistance/Repositories/UserLoginRepository.cs
@@ -0,0 +1,14 @@
+﻿using Application.Services.Repositories;
+using Core.Persistence.Repositories;
+using Core.Security.Entities;
+using Persistance.Context;
+
+namespace Persistance.Repositories
+{
+    public class UserLoginRepository : EfRepositoryBase<UserLogin, int, EFDbContext>, IUserLoginRepository
+    {
+        public UserLoginRepository(EFDbContext context) : base(context)
+        {
+        }
+    }
+}
