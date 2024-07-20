6a9ff985127cf3ae581ccc5733219d99eed42566
esquetta <esquetta@gmail.com>
2024-07-20 18:24:31 +0300

    GithubLogin url added.

--- Infrastructure/Infrastructure/Constants/ExternalAPIUrls.cs
+++ Infrastructure/Infrastructure/Constants/ExternalAPIUrls.cs
@@ -4,6 +4,7 @@
     {
         public const string TwitterAccessToken = "https://api.twitter.com/oauth/access_token";
         public const string TwitterUserInfo = "https://api.twitter.com/1.1/account/verify_credentials.json?include_email=true";
+        public const string TwitterRequestToken = "https://api.twitter.com/oauth/request_token";
 
         public const string GithubAccessToken = "https://github.com/login/oauth/access_token";
         public const string GithubUserInfo = "https://api.github.com/user";
