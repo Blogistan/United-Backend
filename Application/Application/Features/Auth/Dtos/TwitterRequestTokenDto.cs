18d7f18c7e1fccf140de454f0b7bb732143ab55f
esquetta <esquetta@gmail.com>
2024-07-20 18:24:13 +0300

    TwitterRequestTokenDto added.

---
+++ Application/Application/Features/Auth/Dtos/TwitterRequestTokenDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Features.Auth.Dtos
+{
+    public class TwitterRequestTokenDto
+    {
+        public string oauth_token { get; set; }
+        public string oauth_token_secret { get; set; }
+        public bool oauth_callback_confirmed { get; set; }
+    }
+}
