5c3d025b34789cb9c2734925cbc7c99eab1b3ece
esquetta <esquetta@gmail.com>
2024-07-30 21:05:18 +0300

    Code refactoring.

--- Application/Application/Features/Blogs/Queries/GetListBlog/GetListBlogQuery.cs
+++ Application/Application/Features/Blogs/Queries/GetListBlog/GetListBlogQuery.cs
@@ -10,13 +10,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Application.Features.Blogs.Queries.GetListBlog
 {
-    public class GetListBlogQuery:IRequest<GetListBlogQueryResponse>/*,ISecuredRequest*/
+    public class GetListBlogQuery : IRequest<GetListBlogQueryResponse>/*,ISecuredRequest*/
     {
         public PageRequest PageRequest { get; set; }
 
         //public string[] Roles => new string[] { "Admin", "Moderator", "Blogger", "User", "Guest" };
 
-        public class GetListBlogQueryHandler:IRequestHandler<GetListBlogQuery, GetListBlogQueryResponse>
+        public class GetListBlogQueryHandler : IRequestHandler<GetListBlogQuery, GetListBlogQueryResponse>
         {
             private readonly IBlogRepository blogRepository;
             private readonly IMapper mapper;
@@ -28,8 +28,8 @@ namespace Application.Features.Blogs.Queries.GetListBlog
 
             public async Task<GetListBlogQueryResponse> Handle(GetListBlogQuery request, CancellationToken cancellationToken)
             {
-                IPaginate<Blog> paginate = await blogRepository.GetListAsync(index: request.PageRequest.Page, size: request.PageRequest.PageSize, include: x => (Microsoft.EntityFrameworkCore.Query.IIncludableQueryable<Blog, object>)x.Include(x => x.Category).Include(x => x.Writer).ThenInclude(x=>x.Bans).Where(blog=>blog.Writer!=null && (blog.Writer.Bans==null || !blog.Writer.Bans.Any(ban => ban.IsPerma || (ban.BanEndDate != null && ban.BanEndDate < DateTime.UtcNow)))));
 
+                IPaginate<Blog> paginate = await blogRepository.GetListAsync(index: request.PageRequest.Page, size: request.PageRequest.PageSize, include: x => x.Include(x => x.Category).Include(x => x.Writer).ThenInclude(x => x.Bans));
 
                 GetListBlogQueryResponse response = mapper.Map<GetListBlogQueryResponse>(paginate);
 
