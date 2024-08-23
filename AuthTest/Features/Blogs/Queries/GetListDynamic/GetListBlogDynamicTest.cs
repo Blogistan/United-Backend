e8db4bf2a9ebf280e47b024130a4d2d954e31493
esquetta <esquetta@gmail.com>
2024-08-23 16:11:02 +0300

    Missing endpoint added.

--- AuthTest/Features/Blogs/Queries/GetListDynamic/GetListBlogDynamicTest.cs
+++ AuthTest/Features/Blogs/Queries/GetListDynamic/GetListBlogDynamicTest.cs
@@ -1,7 +1,6 @@
 ﻿using Application.Features.Blogs.Queries.GetListBlogDynamic;
 using AuthTest.Mocks.FakeDatas;
 using AuthTest.Mocks.Repositories;
-using FluentValidation.TestHelper;
 using static Application.Features.Blogs.Queries.GetListBlogDynamic.GetListBlogDynamicQuery;
 
 namespace AuthTest.Features.Blogs.Queries.GetListDynamic
@@ -9,19 +8,17 @@ namespace AuthTest.Features.Blogs.Queries.GetListDynamic
     public class GetListBlogDynamicTest: BlogMockRepository, IClassFixture<Startup>
     {
         private readonly GetListBlogDynamicQuery _query;
-        private readonly GetListBlogDynamicQueryValidator _validator;
         private readonly GetListBlogDynamicQueryHandler _handler;
         public GetListBlogDynamicTest(BlogFakeData fakeData) : base(fakeData)
         {
             this._query = new GetListBlogDynamicQuery();
-            this._validator = new GetListBlogDynamicQueryValidator();
             this._handler = new GetListBlogDynamicQueryHandler(MockRepository.Object, Mapper);
         }
-        [Fact]
-        public async Task ThrowExceptionIfPageRequestIsEmpty()
-        {
-            TestValidationResult<GetListBlogDynamicQuery> testValidationResult = _validator.TestValidate(_query);
-            testValidationResult.ShouldHaveValidationErrorFor(x => x.PageRequest);
-        }
+        //[Fact]
+        //public async Task ThrowExceptionIfPageRequestIsEmpty()
+        //{
+        //    TestValidationResult<GetListBlogDynamicQuery> testValidationResult = _validator.TestValidate(_query);
+        //    testValidationResult.ShouldHaveValidationErrorFor(x => x.PageRequest);
+        //}
     }
 }
