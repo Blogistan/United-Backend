bfa32e70681a0e3aad5400db65db333dd0554877
esquetta <esquetta@gmail.com>
2024-10-07 18:57:26 +0300

    Code refactoring.

--- Persistance/Persistance/EntityConfigurations/CategoryConfiguration.cs
+++ Persistance/Persistance/EntityConfigurations/CategoryConfiguration.cs
@@ -8,8 +8,8 @@ namespace Persistance.EntityConfigurations
     {
         public void Configure(EntityTypeBuilder<Category> builder)
         {
-            Category[] categories = { new(1, "Internet"), new(2,"Sosyal Medya"), new(3,"Yazılım"), new(4,"Oyun"), new(5,"Mobil Oyun"), new(7,"Yaşam"), new(8,"Sektörel"),
-                                        new(9,"Otomobil"), new(10,"Yapay Zeka"), new(11,"Sinema ve Dizi"), new(12,"Bilim")};
+            Category[] categories = { new(1, "Internet"), new(2,"Sosyal Medya"), new(3,"Yazılım"), new(4,"Oyun"), new(5,"Mobil Oyun"), new(6,"Yaşam"), new(7,"Sektörel"),
+                                        new(8,"Otomobil"), new(9,"Yapay Zeka"), new(10,"Sinema ve Dizi"), new(11,"Bilim")};
             builder.HasData(categories);
         }
     }
