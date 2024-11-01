2968e2ebffa3a8df89dfb97f1fa672a08bb4c6fe
esquetta <esquetta@gmail.com>
2024-11-01 19:16:20 +0300

    BanException added.

---
+++ corePackages/Core.CrossCuttingConcerns/Core.CrossCuttingConcerns/Exceptions/HttpProblemDetails/BanProblemDetails.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.CrossCuttingConcerns.Exceptions.HttpProblemDetails;
+
+internal class BanProblemDetails : ProblemDetails
+{
+    public BanProblemDetails(string detail)
+    {
+        Title = "Authorization error";
+        Detail = detail;
+        Status = StatusCodes.Status403Forbidden;
+        Type = "https://example.com/probs/authorization";
+    }
+}
+
---
+++ corePackages/Core.CrossCuttingConcerns/Core.CrossCuttingConcerns/Exceptions/Types/BanException.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Types;
+
+public class BanException : Exception
+{
+    public BanException() { }
+
+    protected BanException(SerializationInfo info, StreamingContext context)
+        : base(info, context) { }
+
+    public BanException(string? message)
+        : base(message) { }
+
+    public BanException(string? message, Exception? innerException)
+        : base(message, innerException) { }
+}
+
