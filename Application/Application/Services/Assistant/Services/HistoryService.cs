13bde936762378040090d7f6df5bc43f5be8a8d8
esquetta <esquetta@gmail.com>
2025-04-23 16:24:18 +0300

    History service added for ai service.

---
+++ Application/Application/Services/Assistant/Services/HistoryService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.SemanticKernel.ChatCompletion;
+
+namespace Application.Services.Assistant.Services
+{
+    public static class HistoryService
+    {
+        private static readonly Dictionary<string, ChatHistory> chatHistories = new();
+
+        public static ChatHistory GetChatHistory(string connectionId)
+        {
+            ChatHistory chatHistory = null;
+            if (chatHistories.TryGetValue(connectionId, out chatHistory))
+                return chatHistory;
+            else
+            {
+                chatHistory = new();
+                chatHistories.Add(connectionId, chatHistory);
+            }
+            return chatHistory;
+        }
+    }
+}
