a17b30493431258e04346e2f1ab17284146d20a6
esquetta <esquetta@gmail.com>
2025-04-23 17:07:26 +0300

    Endpoint updated signalR services.

--- Application/Application/Features/Ai/Queries/GeneratePromptResponse/GeneratePromptResponseQuery.cs
+++ Application/Application/Features/Ai/Queries/GeneratePromptResponse/GeneratePromptResponseQuery.cs
@@ -1,24 +1,24 @@
-﻿using Application.Services.Assistant.Services;
+﻿using Infrastructure.OpenAI.Abstract;
 using MediatR;
 
 namespace Application.Features.Ai.Queries.GeneratePromptResponse
 {
-    public class GeneratePromptResponseQuery : IRequest<Unit>
+    public class GeneratePromptResponseQuery : IRequest<GeneratePromptResponseQueryResponse>
     {
         public string Prompt { get; set; }
-        public string ConnectionId { get; set; }
 
-        public class GeneratePromptResponseQueryHandler: IRequestHandler<GeneratePromptResponseQuery, Unit>
+        public class GeneratePromptResponseQueryHandler : IRequestHandler<GeneratePromptResponseQuery, GeneratePromptResponseQueryResponse>
         {
-            private readonly AiService aiService;
-            public GeneratePromptResponseQueryHandler(AiService aiService)
+            private readonly IAiService aiService;
+            public GeneratePromptResponseQueryHandler(IAiService aiService)
             {
-                this.aiService = aiService;;
+                this.aiService = aiService;
             }
-            public async Task<Unit> Handle(GeneratePromptResponseQuery request, CancellationToken cancellationToken)
+            public async Task<GeneratePromptResponseQueryResponse> Handle(GeneratePromptResponseQuery request, CancellationToken cancellationToken)
             {
-                await aiService.GetMessageStreamAsync(request.Prompt, request.ConnectionId,cancellationToken);
-                return Unit.Value;
+                var message = await aiService.GenerateResponse(request.Prompt);
+
+                return new GeneratePromptResponseQueryResponse { Message = message };
             }
         }
     }
