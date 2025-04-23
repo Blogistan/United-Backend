2d037ac804967e954159a98b0c52ee229a1910e5
esquetta <esquetta@gmail.com>
2025-04-23 16:24:26 +0300

    AdiService added.

---
+++ Application/Application/Services/Assistant/Services/AiService.cs
@@ -0,0 +1,32 @@
+﻿using Application.Services.Assistant.Hubs;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+namespace Application.Services.Assistant.Services
+{
+    public class AiService(IHubContext<AiHub> hubContext, IChatCompletionService chatCompletionService, Kernel kernel)
+    {
+        public async Task GetMessageStreamAsync(string promt, string connectionId, CancellationToken? cancellationToken = default!)
+        {
+            PromptExecutionSettings promptExecutionSettings = new()
+            {
+                FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+            };
+
+            var history = HistoryService.GetChatHistory(connectionId);
+            history.AddSystemMessage("You are a creative and helpful blog writing assistant. You produce sample content on blog topics provided by the user, comment on posts, do style analysis and suggest SEO tags.\r\n\r\nYour content should be original, simple and impressive. You can write humorously when necessary and seriously when necessary.");
+            history.AddUserMessage(promt);
+            string responseContent = "";
+
+            await foreach (var response in chatCompletionService.GetStreamingChatMessageContentsAsync(history, promptExecutionSettings, kernel))
+            {
+                cancellationToken?.ThrowIfCancellationRequested();
+                await hubContext.Clients.Client(connectionId).SendAsync(response.ToString());
+                responseContent += response.ToString();
+            }
+            history.AddAssistantMessage(responseContent);
+
+        }
+    }
+}
