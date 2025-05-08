f12b6fccedf99d7cacb86261db1721a7cd164092
esquetta <esquetta@gmail.com>
2025-05-08 20:24:48 +0300

    Add speech-to-text endpoint in AiController
    Implemented a new `SpeechToText` method in `AiController.cs` to handle audio file uploads and convert speech to text. Added necessary namespaces and included error handling for file validation.

--- WebAPI/UnitedAPI/Controllers/AiController.cs
+++ WebAPI/UnitedAPI/Controllers/AiController.cs
@@ -1,4 +1,6 @@
 ﻿using Application.Features.Ai.Queries.GeneratePromptResponse;
+using Application.Features.Ai.Queries.STT;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace UnitedAPI.Controllers
@@ -12,6 +14,21 @@ namespace UnitedAPI.Controllers
         {
             await Mediator.Send(generatePromptResponseQuery);
             return Ok(new { Message = "Prompt received." });
+        }
+        [HttpPost("speech-to-text")]
+        public async Task<IActionResult> SpeechToText([FromBody] IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return BadRequest("Didn't send sound file.");
+
+            using var memoryStream = new MemoryStream();
+            await file.CopyToAsync(memoryStream);
+            var audioBytes = memoryStream.ToArray();
+
+            var query = new SpeechToTextQuery { AudioBytes = audioBytes };
+            var result = await Mediator.Send(query);
+
+            return Ok(result);
         }
     }
 }
